14df0792879fe9c29cd02106c9b9870f028207b3
Ritik <rvjaat25@gmail.com>
2024-09-11 12:16:56 +0530

    page-done

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,7 @@ nextButton.addEventListener("click", () => {
   });
 });
 function openNav() {
-    document.getElementById("navbar").classList.toggle("start-0");
-    document.body.classList.toggle("overflow_hidden");
-    document.querySelector(".menu").classList.toggle("cross")
+  document.getElementById("navbar").classList.toggle("start-0");
+  document.body.classList.toggle("overflow_hidden");
+  document.querySelector(".menu").classList.toggle("cross")
 }
